66968256abfd5ac970fbd84dbcd684b3efd33442
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-03-27 17:30:40 +0430

    Added a very simple redux test

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import './app.css';
 import * as $ from 'jquery';
 import * as HelloWorld from './HelloWorld'
+import { createStore } from 'redux'
 
 console.log('Application bootstraping...');
 
@@ -10,6 +11,30 @@ $(() => {
     $('#get-started-button').on('click', () => {
         alert(HelloWorld.getMessage());
     })
+
+    interface ReduxAction {
+        type: string,
+        payload: object
+    }
+
+    function counter(state = 0, action: ReduxAction) {
+        switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+        default:
+            return state
+        }
+    }
+
+    let store = createStore(counter)
+    store.subscribe(() => console.log(store.getState()))
+    store.dispatch({type: 'INCREMENT'})
+    store.dispatch({type: 'INCREMENT'})
+    store.dispatch({type: 'INCREMENT'})
+    store.dispatch({type: 'DECREMENT'})
+    store.dispatch({type: 'DECREMENT'})
 });
 
 console.log('Bootstrap finished.');
